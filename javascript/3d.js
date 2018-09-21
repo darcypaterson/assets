4be8a097ff3a3017f7f60f2c47b6de5ec1027b87
Darcy Paterson <darcypaterson@gmail.com>
2018-09-21 11:35:43 -0600

    new js

--- javascript/3d.js
+++ javascript/3d.js
@@ -12,7 +12,6 @@
 			camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 			// camera2 = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 			
-// 			camera = new THREE.CinematicCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );		
 
 			scene.background = new THREE.Color( 0x4893AA );
 			
@@ -20,11 +19,6 @@
 
 
 			// Polygons
-
-			// var ngeometry = new THREE.BoxGeometry( 1, 1, 1 );
-			// var nmaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-			// var ncube = new THREE.Mesh( ngeometry, nmaterial );
-			// scene.add( ncube );
 
 
 
@@ -520,11 +514,6 @@
 			// Camera
 
 			camera.position.set( -30, 0, -50 );
-			// camera2.position.set(0.5, player.height, -5);
-			// camera.lookAt(new THREE.Vector3(0, player.height, 0));
-
-// 				camera.setLens( 5 );
-// 				camera.position.set( 2, 1, 50 );
 
 
 			// Lighting
@@ -534,63 +523,14 @@
 			directionalLight.position.set( -12, 10, -8 );
 			scene.add( directionalLight );
 
-			// var directionalLightTwo = new THREE.DirectionalLight( 0xffffff, 1 );
-			// directionalLightTwo.position.set( -12, 5, 3 );
-			// scene.add( directionalLightTwo );
-
 			var light = new THREE.PointLight( 0xffffff, 1.5, 0, 2 );
 			light.position.set( 0, 8, 0.5 );
 			scene.add( light );
 
-			// var light2 = new THREE.PointLight( 0xffffff, 0.85, 50, 2 );
-			// light2.position.set( -4.55, 0.2, 0.18 );
-			// scene.add( light2 );
-
-
-			// var ambLight = new THREE.AmbientLight( 0xffffff, 0.5 ); // soft white light
-			// ambLight.position.set( 3, 2, 0 );
-			// scene.add( ambLight );
-
-			// var hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-			// scene.add( hemiLight );
-
-			// var hemiHelper = new THREE.HemisphereLightHelper( hemiLight, 5 );
-			// scene.add( hemiHelper );
-
-
-			// var spotLight = new THREE.SpotLight( 0xffffff, 0.7 );
-			// spotLight.position.set( -10, 12, -5 );
-			// spotLight.castShadow = true;
-			// spotLight.shadow.mapSize.width = 500;
-			// spotLight.shadow.mapSize.height = 500;
-			// spotLight.shadow.camera.near = .25;
-			// spotLight.shadow.camera.far = 1000;
-			// spotLight.shadow.camera.fov = 3;
-			// scene.add( spotLight );
 
 
 			var sphereSize = 1;
 
-			// var spotLightHelper = new THREE.SpotLightHelper( spotLight, sphereSize );
-			// scene.add( spotLightHelper );
-
-
-			// var pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
-			// scene.add( pointLightHelper );
-
-			// var pointLightHelper2 = new THREE.PointLightHelper( light2, 0.01 );
-			// scene.add( pointLightHelper2 );
-
-
-			// var dirhelper = new THREE.DirectionalLightHelper( directionalLight, 2 );
-			// scene.add( dirhelper );
-
-			// var dirhelperTwo = new THREE.DirectionalLightHelper( directionalLightTwo, 2 );
-			// scene.add( dirhelperTwo );
-
-
-			// var axesHelper = new THREE.AxesHelper( 5 );
-			// scene.add( axesHelper );
 
 
 
@@ -616,9 +556,28 @@
 			});
 
 
-			
+			var yAxis = 20;
+						
 			controls = new THREE.OrbitControls( camera, renderer.domElement );
-			camera.lookAt(new THREE.Vector3( 0, 20, 0 ));
+			camera.lookAt(new THREE.Vector3( 0, yAxis, 0 ));
+
+			window.addEventListener('click', function() {
+
+				if ( yAxis <= 20 ) {
+
+					yAxis -= 1;
+					console.log(yAxis);
+
+				} else if ( yAxis = 0 ) {
+
+					yAxis = 0;
+					console.log(yAxis);
+				}
+
+
+			});
+
+
 
 
 			function animate() {
