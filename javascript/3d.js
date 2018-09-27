fb5fab6b1033c9979c19d2404e3e221bd6f75975
Darcy Paterson <darcypaterson@gmail.com>
2018-09-27 14:21:26 -0600

    new js

--- javascript/3d.js
+++ javascript/3d.js
@@ -8,7 +8,7 @@
 		var keyboard = {};
 		var player = { height: 0.8125, speed: 0.1, turnSpeed: Math.PI * 0.02 };
 
-		cameraInCar = {x: -6.3, y: 0.8125, z: .5 };
+		cameraInCar = {x: -6.35, y: 0.8125, z: .5 };
 		cameraInCarLook = {x: 36.4, y: 0.8125, z: .5 };
 
 		function init() {
