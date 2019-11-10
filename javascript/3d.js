49a9b3255dbdf4a3896c83dd3bae1c22b0554138
Darcy Paterson <darcypaterson@gmail.com>
2019-11-10 11:31:43 -0700

    Update 3d.js

--- javascript/3d.js
+++ javascript/3d.js
@@ -130,7 +130,7 @@
 
 			
 			var loader = new THREE.GLTFLoader(loadingManager);
-			loader.load("scenes/bp_scene_full.gltf", function ( hwy ) {
+			loader.load("scenes/eastridge-4.gltf", function ( hwy ) {
 
 				highway = hwy.scene;
 			    scene.add( hwy.scene );
